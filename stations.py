e69d97d75d734c2e1c04a2081168b007864be9e9
mjames-upc <mjamesis@gmail.com>
2014-04-10 11:18:55 -0600

    padding correction

--- stations.py
+++ stations.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
+# Easy GEMPAK station table from NCAR master list.
+# ---
+# M.James/Unidata	04/14	Created
+#
+#    input
+#       AK ANCHORAGE INTL   PANC  ANC   70273  61 10N  150 01W   38   X     T  X  A    5 US
+#    output
+#       PANC     702730 ANCHORAGE INTL                   AK US  6117 -15002    38  0
+
 import sys,os
 import re
 
 #
-# UNCOMMENT TO DOWNLOAD LATEST FROM UCAR.EDU
+# UNCOMMENT to download latest.  file current as of 4/14 is included with git.
 #os.system('wget http://weather.rap.ucar.edu/surface/stations.txt')
 
-# CONVERT AND FORMAT
+# CONVERT and format for coordinate decimal string
 def dm2dec(degStr):
+  # input string in the format 'degree,minute,direction' e.g '
   (degree, minute, dir) = re.split(',', degStr, maxsplit=3)
-  if re.match('[SW]', degStr): 
+  if re.match('[SW]', degStr):
     sign = -1
   else:
     sign = 1
@@ -42,9 +52,12 @@ with open('stations.txt') as f:
       # left pad longitude for right-aligned column
       if len(str(lonDec)) < 6: 
         lonDec = ' ' + str(lonDec)
+      if len(synopString) < 6: 
+        synopString = ' ' + synopString
 
       # PRINT GEMPAK station table
       print stationID + '     ' + synopString + ' ' + stationName + '                 ' + stateID + ' ' + countryID + '  ' + str(latDec) + ' ' + str(lonDec) + ' ' + elevation + ' 0'
+
 sys.exit(0)
 
 
