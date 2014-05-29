2eaf0795fe996572528185912585d94732f6a1cd
mjames-upc <mjames@unidata.ucar.edu>
2014-05-29 12:14:49 -0600

    shortened if/elses and string replacement

--- stations.py
+++ stations.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # Easy GEMPAK station table from NCAR master list.
+#
+# use: python stations.py stations.txt > output_filename
 # ---
 # M.James/Unidata	04/14	Created
+#			05/15	Take input file from arg
 #
 #    input
 #       AK ANCHORAGE INTL   PANC  ANC   70273  61 10N  150 01W   38   X     T  X  A    5 US
@@ -19,37 +22,26 @@ import re
 def dm2dec(degStr):
   # input string in the format 'degree,minute,direction' e.g '
   (degree, minute, dir) = re.split(',', degStr, maxsplit=3)
-  if re.match('[SW]', dir):
-    sign = -1
-  else:
-    sign = 1
+  sign=(-1 if re.match('[SW]', dir) else 1) #python <2.5 won't like this...
   return str(int(round(sign * (float(degree) + float(minute) / 60 ),2)*100))
 
 fname=sys.argv
-with open('stations.txt') as f:
+with open(fname[1]) as f:
   for line in f:
     if len(line) > 50 and line[0] != "!" and not line.startswith("CD") and line[2] == " ":
-      stateID=line[0:2]
-      if stateID=="  ": stateID="--"
-      countryID=line[81:83]
-      if countryID=="CO": countryID="CB"
+      stateID=line[0:2].replace("  ","--")
+      countryID=line[81:83].replace("CO","CB")
       elevation=line[54:60]
       stationName=line[3:19]
       stationID=line[20:24]
       synopID=line[32:37]
 
       # right pad zero or missing string
-      if synopID.isdigit():
-        synopString=str(int(synopID) * 10)
-      else:
-        synopString="999999"
+      synopString=(str(int(synopID) * 10) if synopID.isdigit() else "999999")
 
       # CONVERT DegMin to Decimal
-      #print line[39:41] + ':' + line[42:44] + ':' + line[44]
-      #print line[47:50] + ':' + line[51:53] + ':' + line[53]
       ( latDegree, latMinute, latDirection ) = ( line[39:41], line[42:44], line[44] )
       ( lonDegree, lonMinute, lonDirection ) = ( line[47:50], line[51:53], line[53] ) 
-
       latDec = dm2dec(latDegree+','+latMinute+','+latDirection)
       lonDec = dm2dec(lonDegree+','+lonMinute+','+lonDirection)
 
@@ -57,5 +49,3 @@ with open('stations.txt') as f:
       print stationID + '     ' + synopString.rjust(6) + ' ' + stationName + '                 ' + stateID + ' ' + countryID + ' ' + str(latDec).rjust(5) + ' ' + str(lonDec).rjust(6) + ' ' + elevation + ' 0'
 
 sys.exit(0)
-
-
